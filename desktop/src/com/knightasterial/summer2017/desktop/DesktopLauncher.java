4a08db9d042ffc6d88dcc08c841b6831d827dfe0
Ryan@Home <ryanzhao@live.com>
2017-06-16 10:48:53 -0400

    Initial Commit / libGDX Generation

---
+++ desktop/src/com/knightasterial/summer2017/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.knightasterial.summer2017.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.knightasterial.summer2017.SummerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SummerGame(), config);
+	}
+}
